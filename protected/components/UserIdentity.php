df201c00b6856e67c4d490aa709e0b03be7cb928
Xiao <xiaosizheme@gmail.com>
2015-09-23 11:22:59 +0800

    Update UserIdentity.php

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -19,8 +19,7 @@ class UserIdentity extends CUserIdentity
 	{
 		$users=array(
 			// username => password
-			'demo'=>'demo',
-			'GigaScience'=>'GigaScience2015',
+		
 		);
 		if(!isset($users[$this->username]))
 			$this->errorCode=self::ERROR_USERNAME_INVALID;
